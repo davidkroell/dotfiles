e5f2a1068829aa8c0feaae577a2e89ccb837a03a
David Kröll <david.kroell@outlook.com>
2023-10-14 14:33:05 +0200

    Add default programs

--- setup-default-programs.sh
+++ setup-default-programs.sh
@@ -8,10 +8,15 @@ fi
 
 
 echo "Installing default programs"
-PROGRAMS="zsh"
+PROGRAMS="zsh clang clang-format clang-tools gcc cloc glibc-doc git bat awscli nmap tree valgrind"
 
 apt install -y $PROGRAMS 
 
 echo "Set ZSH as default shell"
 chsh -s $(which zsh)
+
+
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
