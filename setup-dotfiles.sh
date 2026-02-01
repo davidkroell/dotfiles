f2c6fab902ecedfe34427b1f9f68629352a702b2
David Kröll <david.kroell@outlook.com>
2026-02-01 15:21:14 +0100

    add alacritty and tmux

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -8,7 +8,8 @@ mv $HOME/.zshrc ./olddotfiles/
 mv $HOME/.vimrc ./olddotfiles/
 mv $HOME/.config/starship.toml ./olddotfiles/
 mv $HOME/.gitconfig ./olddotfiles/
-
+mv $HOME/.alacritty.toml ./olddotfiles/
+mv $HOME/.tmux.conf ./olddotfiles/
 
 echo "Creating symlinks from your home dir to the current directory"
 
@@ -19,7 +20,8 @@ ln -s $PWD/.zshrc $HOME/.zshrc
 ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/starship.toml $HOME/.config/starship.toml
 ln -s $PWD/.gitconfig $HOME/.gitconfig
-
+ln -s $PWD/.alacritty.toml $HOME/.alacritty.toml
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 
 echo "Backing up known_hosts and linking it"
 
